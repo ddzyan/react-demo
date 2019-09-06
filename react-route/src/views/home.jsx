ffe08a7d5d7650d4a5371e096aa443a6e9889527
丁东照 <dingdz@sanlogic.com>
2019-09-06 11:10:58 +0800

    完成rect-router的使用demo

--- react-route/src/views/home.jsx
+++ react-route/src/views/home.jsx
@@ -5,6 +5,14 @@ import MyNavLink from '../components/my-nav-link';
 import News from './news';
 import Message from './message';
 class Home extends Component {
+  back = () => {
+    this.props.history.goBack();
+  };
+
+  forward = () => {
+    this.props.history.goForward();
+  };
+
   render() {
     const { path } = this.props.match;
     return (
@@ -14,14 +22,18 @@ class Home extends Component {
         </div>
         <div>
           <MyNavLink to={`${path}/news`}>news</MyNavLink>
-          <MyNavLink to={`${path}/home`}>message</MyNavLink>
+          <MyNavLink to={`${path}/message`}>message</MyNavLink>
         </div>
         <div>
           <Switch>
             <Route path={`${path}/news`} component={News}></Route>
-            <Route path={`${path}/home`} component={Message}></Route>
+            <Route path={`${path}/message`} component={Message}></Route>
             <Redirect to={`${path}/news`}></Redirect>
           </Switch>
+        </div>
+        <div>
+          <button onClick={this.back}>后退</button>
+          <button onClick={this.forward}>前进</button>
         </div>
       </div>
     );
