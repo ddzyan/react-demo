8c67b5b9d448237b117d1e3c436099178d3835f7
丁东照 <dingdz@sanlogic.com>
2019-09-10 11:11:02 +0800

    fist commit

---
+++ react_admin/src/app.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  state = {};
+  render() {
+    return <div>app 组件</div>;
+  }
+}
+
+export default App;
---
+++ react_admin/src/index.js
@@ -0,0 +1,11 @@
+/*
+ * @Author: dingdongzhao
+ * @Date: 2019-09-10 11:09:46
+ * @Last Modified by:   dingdongzhao
+ * @Last Modified time: 2019-09-10 11:09:46
+ */
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './app';
+ReactDOM.render(<App />, document.getElementById('root'));
