96b07d15bfb827ec3cdf8b9f2a5e3d4689e8182f
dingdz <ddzyan@163.com>
2019-09-26 16:31:49 +0800

    修改connect 传入参数

--- react-redux/src-counter-react-redux/containers/app.js
+++ react-redux/src-counter-react-redux/containers/app.js
@@ -1,13 +1,23 @@
-import { connect } from 'react-redux';
+import { connect } from "react-redux";
 
-import { add, minus, asyncAdd } from '../redux/actions';
-import counter from '../components/counter';
+import { add, minus, asyncAdd } from "../redux/actions";
+import counter from "../components/counter";
+
+function mapStateToProps(state) {
+  return {
+    count: state
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    add: number => dispatch(add(number)),
+    minus: number => dispatch(minus(number)),
+    asyncAdd: number => dispatch(asyncAdd(number))
+  };
+}
 
 export default connect(
-  state => ({ count: state }),
-  {
-    add,
-    minus,
-    asyncAdd
-  }
+  mapStateToProps,
+  mapDispatchToProps
 )(counter);
